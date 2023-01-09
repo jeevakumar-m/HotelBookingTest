886e0ad9525f4682f7aa3298c11cecb1052acae6
Jeevakumar Moorthy <jeevakumar.moorthy@gmail.com>
2023-01-09 17:21:58 +0530

    Corrected maven sure fire configuration

--- src/test/java/com/payconiq/cucumber/Runner/TestRunner.java
+++ src/test/java/com/payconiq/cucumber/Runner/TestRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/"
-        ,glue={"com.payconiq.cucumber.hotelBookingTestStepDefinition"},tags="@DeleteBooking",
+        ,glue={"com.payconiq.cucumber.hotelBookingTestStepDefinition"},tags="",
         plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
 
 )
